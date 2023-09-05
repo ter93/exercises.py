7f22d4109973fcbecddcf814b94b6f68b630699b
ter93 <121604687+ter93@users.noreply.github.com>
2023-09-05 10:45:30 +0200

    Update Tic Tac Toe Game.py

--- Tic Tac Toe Game.py
+++ Tic Tac Toe Game.py
@@ -1,45 +1,52 @@
 Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> import random
-... 
-... print("Welcome to the tic-tac-toe!")
-... print("-----------------------------")
-... 
-... for attempt in range(3):
-...   def blank_board():
-...     print("0, 1, 2")
-...     print("3, 4, 5")
-...     print("6, 7, 8")
-... 
-... blank_board()
-... 
-... player_counter = 0
-... computer_counter = 0
-... 
-... for attempt in range(3):
-...   computer = "X"
-...   player = "O"
-...   player = input(f"Choose the number 0-8 to inset X or O. ")
-...   player_choice = int(player)
-...   print("My choice is ", player_choice)
-...   computer_choice = random.randint(0, 8)
-...   print("Computer choice is ", computer_choice)
-... 
-... if computer == "X":
-...   computer_counter += 1
-... else:
-...   player_counter += 1
-... 
-... if player_counter > computer_counter:
-...   print("You win!")
-... elif player_counter < computer_counter:
-...   print("Computer wins!")
-... else:
-...   print("It's a draw!")
-... 
-... play_again_input = input("Do you want to play again? (yes/no) ")
-... play_again = play_again_input == "yes"
-... 
-... if play_again_input.lower() != "yes":
-...   print("Game over!")
-... else:
+import random
+
+def blank_field():
+    print("field[0], field[1], field[2]")
+    print("field[3], field[4], field[5]")
+    print("field[6], field[7], field[8]")
+
+print("Welcome to the tic-tac-toe!")
+print("-----------------------------")
+
+while True:
+    field = [" " for _ in range(9)]
+    player_counter = 0
+    computer_counter = 0
+
+    for attempt in range(3):
+        blank_field()
+
+        computer = "X"
+        player = "O"
+
+        player_choice = int(input(f"Choose a number 0-8 to insert {player}: "))
+
+        if field[player_choice] == " ":
+            field[player_choice] = player
+            blank_field()
+
+            computer_choice = random.randint(0, 8)
+            while field[computer_choice] != " ":
+                computer_choice = random.randint(0, 8)
+
+
+            field[computer_choice] = computer
+            print("Computer choice is ", computer_choice)
+
+
+    if player_counter > computer_counter:
+        print("You win!")
+    elif player_counter < computer_counter:
+        print("Computer wins!")
+    else:
+        print("It's a draw!")
+
+    play_again_input = input("Do you want to play again? (yes/no) ")
+
+    if play_again_input.lower() != "yes":
+        print("Game over!")
+        break
+    else:
+        print("Let's try again!!")
