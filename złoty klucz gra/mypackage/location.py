150c6ecb19283b412b46eebf1cd5cab8896a558d
ter93 <121604687+ter93@users.noreply.github.com>
2024-03-10 13:02:46 +0100

    Add files via upload

---
+++ złoty klucz gra/mypackage/location.py
@@ -0,0 +1,8 @@
+class Location:
+    def __init__(self, name, article, description):
+        self.name = name
+        self.article = article
+        self.description = description
+        
+        
+
